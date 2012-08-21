c3155101c8c5cb610047260cb5a41af4c94d4df3
Yuqing <yqhuang@thoughtworks.com>
2012-08-21 16:00:25 +0530

    Story #16 - Amir/Yuqing - making test target work now, reload and tomcat related targets pending

---
+++ test/smartpool/DummyTest.java
@@ -0,0 +1,12 @@
+package smartpool;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class DummyTest {
+    @Test
+    public void testDummy() throws Exception {
+        assertTrue(true);
+    }
+}
