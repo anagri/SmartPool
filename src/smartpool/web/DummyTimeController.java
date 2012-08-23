67da1955360eceed89747261c2d5e13406995fe6
Suganthi <suganthk@thoughtworks.com>
2012-08-23 13:58:35 +0530

    #3 - Ming/Suganthi - modified buddies table

--- src/smartpool/web/DummyTimeController.java
+++ src/smartpool/web/DummyTimeController.java
@@ -1,16 +1,20 @@
 package smartpool.web;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.stereotype.*;
+import org.springframework.web.servlet.mvc.*;
 import smartpool.service.DummyTimeService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 public class DummyTimeController implements Controller {
 
-    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
-            throws Exception {
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
 
         String time = (new DummyTimeService()).getTime();
 
