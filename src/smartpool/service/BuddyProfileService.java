591ba24315fbd927f54c94bc3b346c265d4e8dd5
Akshay Mankar <itsakshaymankar@gmail.com>
2012-08-23 17:06:10 +0530

    Story #17 - Prithvi/Akshay - View Buddy Profile - Created controller and domain for buddy

---
+++ src/smartpool/service/BuddyProfileService.java
@@ -0,0 +1,10 @@
+package smartpool.service;
+
+import org.springframework.stereotype.Service;
+import smartpool.domain.BuddyProfile;
+@Service
+public class BuddyProfileService {
+    public BuddyProfile findBuddyProfile(int id) {
+        return null;
+    }
+}
