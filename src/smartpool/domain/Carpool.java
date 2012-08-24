56d43e63c31ddbbdca38f5bdedb5f9847d98c159
Yuqing <yqhuang@thoughtworks.com>
2012-08-24 11:48:04 +0530

    Story #9 - (Yuqing/Scott) - Added required fields to the Carpool domain.

--- src/smartpool/domain/Carpool.java
+++ src/smartpool/domain/Carpool.java
@@ -1,6 +1,7 @@
 package smartpool.domain;
 
 import org.joda.time.LocalDate;
+import org.joda.time.LocalTime;
 
 public class Carpool {
 
@@ -8,11 +9,17 @@ public class Carpool {
     public final LocalDate startDate;
     public final String pickupPoint;
     public final CabType cabType;
+    public final int totalCharge;
+    public final LocalTime officePickupTime;
+    public final Status status;
 
-    public Carpool(String name, LocalDate startDate, String pickupPoint, CabType cabType) {
+    public Carpool(String name, LocalDate startDate, String pickupPoint, CabType cabType, int totalCharge, LocalTime officePickupTime, Status status) {
         this.name = name;
         this.startDate = startDate;
         this.pickupPoint = pickupPoint;
         this.cabType = cabType;
+        this.totalCharge = totalCharge;
+        this.officePickupTime = officePickupTime;
+        this.status = status;
     }
 }
--- src/smartpool/service/CarpoolBuilder.java
+++ src/smartpool/service/CarpoolBuilder.java
@@ -1,9 +1,11 @@
 package smartpool.service;
 
 import org.joda.time.LocalDate;
+import org.joda.time.LocalTime;
 import smartpool.domain.CabType;
 import smartpool.domain.Carpool;
+import smartpool.domain.Status;
 
 public class CarpoolBuilder {
-    public static Carpool CARPOOL_1 = new Carpool("carpool-1", LocalDate.now(), "Diamond District", CabType.COMPANY);
+    public static Carpool CARPOOL_1 = new Carpool("carpool-1", LocalDate.now(), "Diamond District", CabType.COMPANY, 100, LocalTime.now(), Status.RUNNING);
 }
