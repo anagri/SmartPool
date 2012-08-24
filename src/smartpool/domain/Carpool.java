665931f2d79f373be0052b09f1d55066f02a1e20
Yuqing <yqhuang@thoughtworks.com>
2012-08-24 14:42:05 +0530

    Story #9 - Yuqing - Change fields to private, add getter.

--- src/smartpool/domain/Carpool.java
+++ src/smartpool/domain/Carpool.java
@@ -3,17 +3,19 @@ package smartpool.domain;
 import org.joda.time.LocalDate;
 import org.joda.time.LocalTime;
 
+import java.util.ArrayList;
+
 public class Carpool {
+    final String name;
+    final LocalDate startDate;
+    final String pickupPoint;
+    final CabType cabType;
+    final int totalCharge;
+    final LocalTime officePickupTime;
+    final Status status;
+    final ArrayList<BuddyProfile> buddyProfiles;
 
-    public final String name;
-    public final LocalDate startDate;
-    public final String pickupPoint;
-    public final CabType cabType;
-    public final int totalCharge;
-    public final LocalTime officePickupTime;
-    public final Status status;
-
-    public Carpool(String name, LocalDate startDate, String pickupPoint, CabType cabType, int totalCharge, LocalTime officePickupTime, Status status) {
+    public Carpool(String name, LocalDate startDate, String pickupPoint, CabType cabType, int totalCharge, LocalTime officePickupTime, Status status, ArrayList<BuddyProfile> buddyProfiles) {
         this.name = name;
         this.startDate = startDate;
         this.pickupPoint = pickupPoint;
@@ -21,5 +23,38 @@ public class Carpool {
         this.totalCharge = totalCharge;
         this.officePickupTime = officePickupTime;
         this.status = status;
+        this.buddyProfiles = buddyProfiles;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public String getPickupPoint() {
+        return pickupPoint;
+    }
+
+    public CabType getCabType() {
+        return cabType;
+    }
+
+    public int getTotalCharge() {
+        return totalCharge;
+    }
+
+    public LocalTime getOfficePickupTime() {
+        return officePickupTime;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public ArrayList<BuddyProfile> getBuddyProfiles() {
+        return buddyProfiles;
     }
 }
