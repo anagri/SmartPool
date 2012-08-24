07cae29f702b1aa568270ac03581eada2c8fc258
Yuqing <yqhuang@thoughtworks.com>
2012-08-24 09:56:06 +0530

    Story #9 - Scott/Yuqing - change controller to use annotation

--- test/smartpool/web/CarpoolControllerTest.java
+++ test/smartpool/web/CarpoolControllerTest.java
@@ -2,43 +2,43 @@ package smartpool.web;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.ui.ModelMap;
 import smartpool.domain.Carpool;
 import smartpool.service.CarpoolBuilder;
 import smartpool.service.CarpoolService;
 
-import javax.servlet.http.HttpServletRequest;
-
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
-import static org.mockito.MockitoAnnotations.initMocks;
 
+@RunWith(MockitoJUnitRunner.class)
 public class CarpoolControllerTest {
 
     private CarpoolController carpoolController;
     @Mock
-    private HttpServletRequest httpServletRequest;
-    @Mock
     private CarpoolService carpoolService;
+    private ModelMap model;
+    private Carpool expectedCarpool = CarpoolBuilder.CARPOOL_1;
 
     @Before
     public void setUp() throws Exception {
-        initMocks(this);
         carpoolController = new CarpoolController(carpoolService);
-        when(httpServletRequest.getParameter("name")).thenReturn("carpool");
-        when(carpoolService.findCarpoolBy("carpool")).thenReturn(CarpoolBuilder.CARPOOL_1);
+        when(carpoolService.findCarpoolBy("carpool")).thenReturn(expectedCarpool);
+        model = new ModelMap();
     }
 
     @Test
     public void shouldRedirectToViewCarpool() throws Exception {
-        assertEquals(carpoolController.handleRequest(httpServletRequest, null).getViewName(), "carpool/view");
+        assertEquals(carpoolController.viewCarpool("carpool", model), "carpool/view");
     }
 
     @Test
     public void shouldHaveCarpoolInstanceInModel() throws Exception {
-        Carpool carpoolExpected = CarpoolBuilder.CARPOOL_1;
-        Carpool carpoolActual = (Carpool) carpoolController.handleRequest(httpServletRequest, null).getModel().get("carpool");
+        carpoolController.viewCarpool("carpool", model);
+        Carpool carpoolActual = (Carpool) model.get("carpool");
 
-        assertEquals(carpoolActual, carpoolExpected);
+        assertEquals(expectedCarpool, carpoolActual);
     }
 }
