c57af28726b1c42f472658d703acdd1c5a8b3a76
Vitor Franca <vfranca@thoughtworks.com>
2012-08-27 14:43:15 +0530

    Story #49 - Vitor / Prithvi - Functional tests running properly (I hope)

--- test/smartpool/CASTest.java
+++ test/smartpool/CASTest.java
@@ -1,46 +1,39 @@
-//package smartpool;
-//
-//
-//import com.thoughtworks.selenium.SeleneseTestBase;
-//import org.junit.After;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.WebDriverBackedSelenium;
-//import org.openqa.selenium.firefox.FirefoxDriver;
-//
-//public class CASTest extends SeleneseTestBase {
-//
-//    private String homeUrl;
-//    private String serverUrl;
-//
-//    @Before
-//    public void setUp() throws Exception {
-//        WebDriver driver = new FirefoxDriver();
-//        serverUrl = "https://castest.thoughtworks.com/";
-//        homeUrl = "http://localhost:9090/SmartPool/";
-//        selenium = new WebDriverBackedSelenium(driver, serverUrl);
-//    }
-//
-//    @Test
-//    public void shouldRedirectToCASTest() throws Exception {
-//        selenium.open(homeUrl);
-//        verifyEquals(serverUrl, selenium.getLocation());
-//    }
-//
-//    @Test
-//    public void casWrongUsernameWrongPasswordTest() throws Exception {
-//        selenium.open(homeUrl);
-//        selenium.type("id=username", "wrongusername");
-//        selenium.type("id=password", "wrongpassword");
-//        selenium.click("name=submit");
-//        selenium.waitForPageToLoad("30000");
-//        assertTrue(selenium.isTextPresent("exact:The credentials you provided cannot be determined to be authentic.\n"));
-//    }
-//
-//    @After
-//    public void tearDown() throws Exception {
-//        selenium.stop();
-//    }
-//}
-//
+import com.thoughtworks.selenium.Selenium;
+import com.thoughtworks.selenium.SeleneseTestCase;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverBackedSelenium;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.regex.Pattern;
+
+public class CASTest extends SeleneseTestCase {
+	@Before
+	public void setUp() throws Exception {
+		WebDriver driver = new FirefoxDriver();
+		String baseUrl = "https://castest.thoughtworks.com/";
+		selenium = new WebDriverBackedSelenium(driver, baseUrl);
+	}
+
+	@Test
+	public void testWrongUsernameAndPassword() throws Exception {
+		selenium.open("/cas/login?service=http%3A%2F%2Flocalhost%3A9090%2FSmartPool%2F");
+		for (int second = 0;; second++) {
+			if (second >= 60) fail("timeout");
+			try { if (selenium.isElementPresent("id=username")) break; } catch (Exception e) {}
+			Thread.sleep(1000);
+		}
+
+		selenium.type("id=username", "wrongusername");
+		selenium.type("id=password", "wrongpassword");
+		selenium.click("name=submit");
+		selenium.waitForPageToLoad("30000");
+		verifyTrue(selenium.isTextPresent("The credentials you provided cannot be determined to be authentic"));
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		selenium.stop();
+	}
+}
