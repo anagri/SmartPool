bc2dce18b581d304c8b050d733440d1ce40f4559
zmTWer <zm_uestc@hotmail.com>
2012-08-28 14:55:51 +0530

    #17 View Profile -- isha/ming -- Database changed

--- test/smartpool/persistence/dao/BuddyDaoTest.java
+++ test/smartpool/persistence/dao/BuddyDaoTest.java
@@ -6,19 +6,16 @@ import smartpool.domain.Buddy;
 
 public class BuddyDaoTest {
     @Test
-    public void shouldGetBuddyByUserName(){
-        Buddy expectedBuddy = new Buddy("1");
+    public void shouldGetBuddyByUserName() {
+        Buddy expectedBuddy = new Buddy("prithvin");
         BuddyDao buddyDao = new BuddyDao();
-        Assert.assertEquals(expectedBuddy, buddyDao.selectBuddy("1"));
+        Assert.assertEquals(expectedBuddy, buddyDao.selectBuddy("prithvin"));
     }
 
     @Test
-    public void shouldGetFirstBuddy(){
-        Buddy expectedBuddy = new Buddy("1");
+    public void shouldGetFirstBuddy() {
+        Buddy expectedBuddy = new Buddy("arnavku");
         BuddyDao buddyDao = new BuddyDao();
         Assert.assertEquals(expectedBuddy, buddyDao.selectAllBuddies());
     }
-
-
-
 }
