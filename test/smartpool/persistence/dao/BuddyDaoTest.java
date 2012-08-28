303cac0befbfcd400f929858549ce899058ef974
zmTWer <zm_uestc@hotmail.com>
2012-08-28 15:03:51 +0530

    #17 View Profile

--- test/smartpool/persistence/dao/BuddyDaoTest.java
+++ test/smartpool/persistence/dao/BuddyDaoTest.java
@@ -7,18 +7,15 @@ import smartpool.domain.Buddy;
 public class BuddyDaoTest {
     @Test
     public void shouldGetBuddyByUserName(){
-        Buddy expectedBuddy = new Buddy("1");
+        Buddy expectedBuddy = new Buddy("prithvin");
         BuddyDao buddyDao = new BuddyDao();
-        Assert.assertEquals(expectedBuddy, buddyDao.selectBuddy("1"));
+        Assert.assertEquals(expectedBuddy, buddyDao.selectBuddy("prithvin"));
     }
 
     @Test
     public void shouldGetFirstBuddy(){
-        Buddy expectedBuddy = new Buddy("1");
+        Buddy expectedBuddy = new Buddy("arnavku");
         BuddyDao buddyDao = new BuddyDao();
         Assert.assertEquals(expectedBuddy, buddyDao.selectAllBuddies());
     }
-
-
-
 }
