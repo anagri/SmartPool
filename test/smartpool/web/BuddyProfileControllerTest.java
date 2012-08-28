1129752de105b699e8383f5194d44f8e6c62b426
zmTWer <zm_uestc@hotmail.com>
2012-08-28 14:45:23 +0530

    #17 View Profile -- isha/ming -- Database changed

--- test/smartpool/web/BuddyProfileControllerTest.java
+++ test/smartpool/web/BuddyProfileControllerTest.java
@@ -26,19 +26,19 @@ public class BuddyProfileControllerTest {
         buddy = new Buddy("Prithvi");
         buddyProfileController = new BuddyProfileController(buddyProfileService);
 
-        when(buddyProfileService.getBuddy("1")).thenReturn(buddy);
+        when(buddyProfileService.getBuddy("prithvin")).thenReturn(buddy);
     }
 
     @Test
     public void shouldReturnView(){
         ModelMap model = new ModelMap();
-        assertThat(buddyProfileController.viewProfile("1", model),equalTo("buddy/viewProfile"));
+        assertThat(buddyProfileController.viewProfile("prithvin", model),equalTo("buddy/viewProfile"));
     }
 
     @Test
     public void shouldPutDomainObjectInModel(){
         ModelMap model = new ModelMap();
-        buddyProfileController.viewProfile("1", model);
+        buddyProfileController.viewProfile("prithvin", model);
         assertThat((Buddy) model.get("buddyProfile"),equalTo(buddy));
     }
 }
