e2a8763c278d478a9a67f08a9a092d47d9dc938c
mdaliejaz <mdaliejaz@gmail.com>
2012-08-29 15:09:46 +0530

    #51 - Ali/Sam/Luka - Creating dynamic tasks to generate war for each enviroment (dev, qa, uat). War created has the correct urls poked in them. Fixing the CAS hardcoded url issue

--- scripts/deployToQA.sh
+++ scripts/deployToQA.sh
@@ -1,1 +1,1 @@
-sshpass -p '!abcd1234' scp build/libs/smartpool.war root@10.10.15.121:/usr/local/apache-tomcat-7.0.29/webapps/smartpool-test.war
+sshpass -p '!abcd1234' scp build/libs/smartpool-test.war root@10.10.15.121:/usr/local/apache-tomcat-7.0.29/webapps/smartpool-test.war
