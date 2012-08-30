77a6438c9bf88232ca9b51bc7e71f710e7e415cb
arnav90 <arnavk90@gmail.com>
2012-08-30 09:59:22 +0530

    Story # 49 - Hulk/Loki/Beast - creating separate war files for dev, qa and uat

--- scripts/deployToUAT.sh
+++ scripts/deployToUAT.sh
@@ -1,1 +1,1 @@
-sshpass -p '!abcd1234' scp build/libs/smartpool.war root@10.10.15.121:/usr/local/apache-tomcat-7.0.29/webapps/smartpool.war
+sshpass -p '!abcd1234' scp build/libs/smartpool-uat.war root@10.10.15.121:/usr/local/apache-tomcat-7.0.29/webapps/smartpool-uat.war
