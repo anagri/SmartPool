21aaf6893407149cc9b51789a78994054753f3b9
Ayush <ayush@ayusht.(none)>
2012-09-04 13:54:17 +0530

    #00 - Ironman/Ayush/Luke - Tomcat startup script to wait until tomcat starts

--- scripts/startupTomcat.sh
+++ scripts/startupTomcat.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+
 /usr/local/apache-tomcat-7.0.29/bin/startup.sh
+
+until [ "`curl --silent --show-error --connect-timeout 1 -I http://localhost:9090 | grep 'Coyote'`" != "" ];
+do
+  echo --- sleeping for 5 seconds
+  sleep 5
+done
+
+echo Tomcat is ready!
+
+
