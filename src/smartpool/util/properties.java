3c61ca9c60795216040bd46d55e85fcb86718e14
Yuqing <yqhuang@thoughtworks.com>
2012-09-06 11:55:15 +0530

    Story #54 - Isha/Yuqing - Added properties to remove hard code url.

---
+++ src/smartpool/util/properties.java
@@ -0,0 +1,5 @@
+package smartpool.util;
+
+public class properties {
+    public static final String casServer = "https://castest.thoughtworks.com/cas";
+}
--- src/smartpool/web/LogoutController.java
+++ src/smartpool/web/LogoutController.java
@@ -3,6 +3,7 @@ package smartpool.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import smartpool.util.properties;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -12,6 +13,6 @@ public class LogoutController {
     @RequestMapping(value = "/logout", method = RequestMethod.GET)
     public String logout(HttpServletRequest httpRequest){
         httpRequest.getSession().invalidate();
-        return "redirect:https://castest.thoughtworks.com/cas/logout";
+        return "redirect:"+ properties.casServer + "/logout";
     }
 }
