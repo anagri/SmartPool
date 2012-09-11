269e3f896e1f8142f2911887ff5db6da101a4931
prithvin <prithvin@smartpool.prithvi>
2012-09-11 17:53:53 +0530

    00 - Prithvi/Suganthi - Changed test data to correct suganthi's emailid

--- dbscripts/test_data.sql
+++ dbscripts/test_data.sql
@@ -14,7 +14,7 @@ INSERT INTO `buddies` VALUES
 ('nibub','Nibu Baby','#659, Jayanthi Residency, Flat# 301, 1st Main, C Block,AECS Layout, Kundalahalli, Bangalore','nibub@thoughtworks.com','9686190831',NULL,NULL),
 ('prithvin','Prithvi Nambiar','H-81, Diamond District, Bangalore','prithvin@thoughtworks.com','1234567890',NULL,NULL),
 ('ssquire','Samuel Michael Squir','P-81, Diamond District, Bangalore','ssquire@thoughtworks.com','0987654321',NULL,NULL),
-('suganthk','Suganthi T','L-82, Diamond District, Bangalore','suganthit@thoughtworks.com','1234567890',NULL,NULL),
+('suganthk','Suganthi T','L-82, Diamond District, Bangalore','suganthk@thoughtworks.com','1234567890',NULL,NULL),
 ('test.twu','Test User','Somewhere, Bangalore','test@thoughtworks.com','1234567890',NULL,NULL),
 ('vfranca','Vitor Franca','F-75, Diamond District, Bangalore','vfranca@thoughtworks.com','1234567890',NULL,NULL);
 
