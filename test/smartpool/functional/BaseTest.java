6a110bd40c81bddc7e8f9f7509fc4cca7364c8ec
zmTWer <zm_uestc@hotmail.com>
2012-09-12 11:24:58 +0530

    #4 - Govind/Ayush/Luka - Create Profile for test.user happens only if not been setup. This check to create the test.twu profile happens in the setup of each Functional Test.

--- test/smartpool/functional/BaseTest.java
+++ test/smartpool/functional/BaseTest.java
@@ -3,22 +3,42 @@ package smartpool.functional;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import smartpool.functional.page.LoginPage;
 import smartpool.functional.page.Page;
 
 public abstract class BaseTest {
 
     protected FirefoxDriver webDriver;
+    private static boolean TEST_USER_CREATED = false;
 
     @Before
     public void setUp() {
+        createWebDriver();
+        if (!TEST_USER_CREATED) {
+            createProfileForTestUser();
+        }
+    }
+
+    @After
+    public void tearDown() {
+        destroyWebDriver();
+    }
+
+    private void createWebDriver() {
         webDriver = new FirefoxDriver();
         webDriver.manage().window().maximize();
         webDriver.get(Page.BASE_URL);
     }
 
-    @After
-    public void tearDown() {
+    private void destroyWebDriver() {
         webDriver.manage().deleteAllCookies();
         webDriver.quit();
+    }
+
+    private void createProfileForTestUser() {
+        new LoginPage(webDriver).login().myProfileRedirectToCreateProfilePage().enterProfileDetails().verifyDetailsOfNewBuddy("Test User");
+        TEST_USER_CREATED = true;
+        destroyWebDriver();
+        createWebDriver();
     }
 }
